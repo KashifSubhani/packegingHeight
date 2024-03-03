62238ed44846ebd72f484b505a701166511c4ce3
Mian Hassan <161042716+KashifSubhani@users.noreply.github.com>
2024-03-04 02:24:06 +0500

    faqs

--- src/demoData/faqsData.tsx
+++ src/demoData/faqsData.tsx
@@ -1,27 +1,75 @@
 export const faqsData = [
   {
-    question: "What is the process of getting a loan?",
+    question: "Why should I care about eco-friendly packaging?",
     answer:
-      "The process of getting a loan is simple. You can apply for a loan online or visit our office. You will need to provide some basic information about yourself and your business. We will then review your application and let you know if you are approved. If you are approved, we will work",
+      "Eco-friendly packaging helps save the planet! It reduces waste and pollution, which everyone loves.",
   },
   {
-    question: "What is the process of getting a loan?",
+    question:
+      "What's so great about using corrugated card stock for packaging?",
     answer:
-      "The process of getting a loan is simple. You can apply for a loan online or visit our office. You will need to provide some basic information about yourself and your business. We will then review your application and let you know if you are approved. If you are approved, we will work",
+      "It protects your products during shipping and can be recycled afterward, so it's good for the environment too.",
   },
   {
-    question: "What is the process of getting a loan?",
+    question:
+      "How does the design of my packaging affect what people think of my brand?",
     answer:
-      "The process of getting a loan is simple. You can apply for a loan online or visit our office. You will need to provide some basic information about yourself and your business. We will then review your application and let you know if you are approved. If you are approved, we will work",
+      "It tells people what you're all about! A cool design shows you care about quality and style.",
   },
   {
-    question: "What is the process of getting a loan?",
+    question: "Why should I choose rigid boxes for my fancy products?",
     answer:
-      "The process of getting a loan is simple. You can apply for a loan online or visit our office. You will need to provide some basic information about yourself and your business. We will then review your application and let you know if you are approved. If you are approved, we will work",
+      "Rigid boxes are like the red carpet for your products! They make them look super luxurious and valuable, which makes customers feel special.",
   },
   {
-    question: "What is the process of getting a loan?",
+    question:
+      "Can Packaging Height make custom packaging for any type of business?",
     answer:
-      "The process of getting a loan is simple. You can apply for a loan online or visit our office. You will need to provide some basic information about yourself and your business. We will then review your application and let you know if you are approved. If you are approved, we will work",
+      "Absolutely! Whether you sell makeup, gadgets, or even pet toys, we've got you covered with packaging that's as unique as your business.",
+  },
+  {
+    question: "What cool printing options do you offer for custom packaging?",
+    answer:
+      "From digital to offset and even flexographic printing, we'll make sure your packaging looks top-notch.",
+  },
+  {
+    question: "How do I get a custom quote for my packaging needs?",
+    answer:
+      "Easy-peasy! Just hop onto our website and fill out a quick form with all your details. Our super-friendly team will whip up a personalized quote in no time!",
+  },
+  {
+    question: "Can you help with designing my custom packaging?",
+    answer:
+      "Absolutely! Our design wizards are here to sprinkle some magic on your packaging if you need it.",
+  },
+  {
+    question: "Do you offer packaging solutions for small businesses?",
+    answer:
+      "You betcha! Whether you're a one-person show or a growing startup, we've got flexible solutions tailored just for you.",
+  },
+  {
+    question: "Can I check out samples before making a decision?",
+    answer:
+      "Of course! We want you to love your packaging as much as we do. Just say the word, and we'll send over some samples.",
+  },
+  {
+    question: "Why should I bother with custom packaging for my brand?",
+    answer:
+      "Custom packaging boosts your brand's visibility, makes your products look amazing, and gives your customers a jaw-dropping unboxing experience they won't forget!",
+  },
+  {
+    question: "Can you handle rush orders or tight deadlines?",
+    answer:
+      "For those urgent requests, we offer priority turnaround options to ensure your orders are delivered on time, every time.",
+  },
+  {
+    question:
+      "What kinds of industries does Packaging Height specialize in serving?",
+    answer:
+      "We cater to a diverse range of industries, including cosmetics, food and beverage, electronics, retail, and more.",
+  },
+  {
+    question: "How do I contact Packaging Height if I have more questions?",
+    answer: "You can shoot us an email or give us a ring. ",
   },
 ];
