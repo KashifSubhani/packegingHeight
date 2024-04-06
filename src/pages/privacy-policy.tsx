837afd84097622cc4c9280af3019ff5aa9786855
QazziShahid <161294929+QazziShahid@users.noreply.github.com>
2024-04-07 02:53:30 +0500

    Update privacy-policy.tsx

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -19,84 +19,35 @@ const Index = ({ data }: any) => {
       <Container maxWidth={"lg"}>
         <div className="py-10">
           <p className="mt-4 p-0 text-black">
-            Visiting our website <a href="https://packagingheight.com/">packagingheight.com</a> is an acceptance of agreement to the privacy policy described on our website. Refine Packaging knows well and is managing the privacy of users of the website strictly. It is assured that any information which is collected for processing of the order or any other query will not be shared or revealed to any third party user, and will cautiously be used only for customer service, fulfilling shipping request and for contacting the customers for feedback.
+            Visiting our website <a href="https://packagingheight.com/">packagingheight.com</a> is an acceptance of agreement to the privacy policy described on our website. Packaging Height knows well and is managing the privacy of users of the website strictly. It is assured that any information that is collected for the processing of the order or any other query will not be shared or revealed to any third-party user, and will cautiously be used only for customer service, fulfilling shipping requests, and contacting the customers for feedback.
 <br />
 Please understand we will reveal your information when required by law or to protect our interests and safety and those of other visitors to our website, or when you allow us to do so. Your information can also be disclosed when we have a good-faith belief that it is necessary to abide by a current judicial proceeding, a court order or legal process served on our website.
           </p>
-          <h1 className="mt-4 font-bold text-black text-xl">
-            What Is a Terms of Service Agreement?
-          </h1>
+          <h2 className="mt-4 font-bold text-black text-xl">
+           Information Collection
+          </h2>
           <p className="mt-3 p-0 text-black">
-            A terms of service sets all user rules, restrictions, and prohibited
-            behaviors, and outlines your company’s liability limitations,
-            property rights, and dispute resolutions.
-          </p>
-          <p className="mt-3 p-0 text-black">
-            You’ve probably seen some alternative titles for a terms of service
-            agreement, like:
-          </p>
-          <ul className="list-disc mt-8 pl-6 flex flex-col gap-y-3">
-            <li>Terms of Use</li>
-            <li>Terms and Conditions</li>
-            <li>ToS Agreement</li>
-            <li>General Terms</li>
-            <li>Terms</li>
-          </ul>
-
-          <p className="mt-8 p-0 text-black">
-            Despite the different names, these agreements are all the same.
-          </p>
+            We collect information from you when you register on the site, place an order, respond to a survey or communication such as e-mail, or participate in another site feature. When ordering or registering, we may ask you for your name, e-mail address, mailing address, phone number or other information. You may, however, visit our site anonymously. Like many websites, we use “cookies” to enhance your experience and gather information about visitors and visits to our websites. Please refer to the “cookies” section below for information about cookies and how we use them.</p>
+         
+          <h2 className="mt-8 font-bold text-black text-xl">
+            Uses of Information
+          </h2>
           <p className="mt-4 p-0 text-black">
-            No matter what you choose to call it, a generic terms of service
-            policy can help protect your business from users who might break the
-            law or cause harm to your website, app, or other users.
-          </p>
+            We may use the information we collect from you when you register, purchase products, enter a contest or promotion, respond to a survey or marketing communication, surf the website, or use certain other site features in the following ways: To personalize your site experience and to allow us to deliver the type of content and product offerings in which you are most interested. To allow us to better service you in responding to your customer service requests. To quickly process your transactions. To administer a contest, promotion, survey or other site feature. If you have opted-in to receive our e-mail newsletter, we may send you periodic e-mails. If you would no longer like to receive promotional e-mails from us, please refer to the “How can you opt-out, remove or modify information you have provided to us?” section below. If you have not opted-in to receive e-mail newsletters, you will not receive these e-mails. Visitors who register or participate in other site features such as marketing programs and ‘members-only’ content will be given a choice whether they would like to be on our e-mail list and receive e-mail communications from us.<br><br>
+We do not provide, distribute or sell our website users and customers’ information to any other party. The website users and customers’ information can be used for marketing and improvement of the packagingheight.com website, products, services and promotional offers. Please note we also use this information, to contact you regarding important updates on our website and other information we think may be useful and of interest to you.</p>
 
+          <h2 className="mt-8 font-bold text-black text-xl">
+            Information Protection
+          </h2>
           <p className="mt-4 p-0 text-black">
-            You can customize our free terms of service template below,
-            including the title, so it works for you no matter if your own a:
-          </p>
-
-          <ul className="list-disc mt-8 pl-6 flex flex-col gap-y-3">
-            <li>Dropshipping businesses</li>
-            <li>Marketplace shop</li>
-            <li>Retail store</li>
-            <li>Ecommerce website or app</li>
-            <li>Blog</li>
-            <li>Forum</li>
-            <li>Subscription-based platform</li>
-          </ul>
-
-          <h1 className="mt-8 font-bold text-black text-xl">
-            Why You Need a Terms of Service for Your Website or App
-          </h1>
+            The security of our website users and customer's information is of high priority to us. We take all reasonable measures against unauthorized access or use of your personal and business information online and offline. Access to website users and customers’ information is restricted in our offices. Our site uses the Secure Socket Layer (SSL) encryption, which is the highest form of data security system when information is sent or received over the internet. Although SSL encryption verifies the integrity of information and security of sending/receiving servers, information is not 100% safe over the internet. We therefore cannot warrant the security of the information you send us over the Internet.</p>
+          <h2 className="mt-8 font-bold text-black text-xl">
+            Cookies
+          </h2>
           <p className="mt-4 p-0 text-black">
-            While a ToS agreement is not required by law, you need to post one
-            on your website or app to help:
-          </p>
-
-          <ul className="list-disc mt-8 pl-6 flex flex-col gap-y-3">
-            <li>Set your users’ expectations</li>
-            <li>Protect your business from legal liabilities</li>
-            <li>Maintain rights over your intellectual property</li>
-            <li>Minimizes Disputes</li>
-            <li>Establish trust and transparency with your users</li>
-          </ul>
-
-          <h1 className="mt-8 font-bold text-black text-xl">
-            Terms of Service Set Your Users’ Expectations
-          </h1>
-          <p className="mt-4 p-0 text-black">
-            One of the purposes of a terms of service policy is to explain the
-            rules and guidelines your users must follow while accessing your
-            services, which helps set their expectations.
-          </p>
-          <p className="mt-4 p-0 text-black">
-            By clearly communicating what’s allowed and prohibited on your
-            website or app and explaining to your users the consequences of
-            breaking those rules, you make it easier for your business to
-            prevent users from abusing your services or causing harm to others.
-          </p>
+            Do we use cookies? Yes!
+A cookie is actually set of data programmed and stored on your computer for an extended period of time by our website. A cookie will permit you to use our website as a specific individual and relate to your information in our servers/records. Cookies need to be enabled in order to use packagingheight.com website. You can remove cookies by following instructions in your web browser. Each browser is a little different, so look at your browser Help menu to learn the correct way to modify your cookies. If you turn cookies off, you won’t have access to many features that make your site experience more efficient and some of our services will not function properly. However, you can still use many features of our website.</p>
+         
         </div>
       </Container>
       <Footer />
