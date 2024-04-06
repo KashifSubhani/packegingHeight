55ccf7a070a4006f5e994e8c8d3c26dc32d60cb7
QazziShahid <161294929+QazziShahid@users.noreply.github.com>
2024-04-07 03:13:17 +0500

    Update privacy-policy.tsx

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -50,7 +50,7 @@ A cookie is actually set of data programmed and stored on your computer for an e
 
           <div className="py-10  px-3" style={{ background: "#F1F8F1" }}>
         <div>
-          <h3 className="text-4xl fw_600 text-left">Contact US</h3>
+          <p className="text-4xl fw_600 text-left">Contact US</p>
           <p className="mt-4 text-left fw_400">
 <strong>If there are any questions regarding this privacy policy, please call +1(307) 429 2922 or email info@packagingheight.com for assistance.</strong>
           </p>
