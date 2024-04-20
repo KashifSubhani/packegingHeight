6e0667ff69b5125d097966dd8b81df0853b9c7b2
Mian Hassan <161042716+KashifSubhani@users.noreply.github.com>
2024-04-20 21:54:13 +0500

    twk

--- src/components/shared/zohoWidget.tsx
+++ src/components/shared/zohoWidget.tsx
@@ -5,7 +5,7 @@ const ZohoChatWidget = () => {
     const tawk = document.createElement('script');
     tawk.async = true;
     tawk.src = 'https://embed.tawk.to/6623edcda0c6737bd12e8de5/1hru5hr2l';
-    const s = document.getElementsByTagName('script')[0];
+    const s:any = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(tawk, s);
     return () => {
       document.getElementById('tawkToScript')?.remove();
