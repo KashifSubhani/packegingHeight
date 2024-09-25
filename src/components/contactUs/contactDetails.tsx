842feaed62d38d5babc7f4906d4405a2fd28d83a
Mufaqar <mufaqar@gmail.com>
2024-09-25 14:14:55 +0500

    resolve error

--- src/components/contactUs/contactDetails.tsx
+++ src/components/contactUs/contactDetails.tsx
@@ -1,7 +1,7 @@
 import { Container } from "@mui/material";
 import contactBg from "../../static/contactBg.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import {
   faFacebookF,
