b2a8f34d6bca88066f95d5f4a60e4f39eb98b7f7
KashifSubhani <161042716+KashifSubhani@users.noreply.github.com>
2025-04-23 01:09:55 +0500

    Update faqItem.tsx

--- src/components/shared/subcomponents/faqItem.tsx
+++ src/components/shared/subcomponents/faqItem.tsx
@@ -14,7 +14,7 @@ export const FaqItem = (props: any) => {
         expandIcon={<Image src={chev} alt="" width={16} height={16} />}
         className="fw_400 px-6 py-2 text-sm"
       >
-        <h4>{props.item.question}</h4>
+        <h3>{props.item.question}</h3>
       </AccordionSummary>
       <AccordionDetails className="pt-0">
         <div className="mx-3 my-0 border-t border-zinc-200 px-0 pb-0 pt-4 text-sm">
